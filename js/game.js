c9472342c32dec8fa7147bc130983f95972ddcca
jota_sk <jota_sk@hotmail.com>
2016-04-16 16:38:31 +0100

    ultraupdate with bugs

--- js/game.js
+++ js/game.js
@@ -3,58 +3,291 @@
  */
 
 var
+
+    debug = true,
+
     canvas,
     ctx,
     width,
-    height,
 
+    fgpos = 0,
     frames = 0,
     score = 0,
-    best  = 0,
+    best  = localStorage.getItem("best") || 0,
 
-    currentsate,
+    currentstate,
     states = {
         Splash: 0,
         Game: 1,
-        Scores: 2
+        Score: 2
     },
 
     bird = {
 
+        god: false,
+
+        x: 60,
+        y: 0,
+
+        frame: 0,
+        velocity: 0,
+        animation: [0,1,2,1],
+
+        rotation: 0,
+        radius: 12,
+
+        gravity: 0.25,
+        _jump: 4.6,
+
+        jump: function(){
+          this.velocity = -this._jump;
+        },
+
+        update: function(){
+            var n = currentstate === states.Splash ? 10: 5;
+            this.frame += frames % n === 0 ? 1: 0;
+            this.frame %= this.animation.length;
+
+            if(currentstate === states.Splash){
+                this.y = height - 280 + 5 * Math.cos(frames/10);
+                this.rotation = 0;
+            }
+            if(currentstate === states.Game){
+                this.velocity += this.gravity;
+                this. y += this.velocity;
+
+                if(this.y >= height - s_fg.height - 10){
+                    this.y = height - s_fg.height - 10;
+                    if(currentstate === states.Game){
+                        if(!Boolean(this.god)){
+                            currentstate = states.Score;
+                        }
+                    }
+                    this.velocity = this._jump;
+                }
+
+                if(this.velocity >= this._jump){
+                    this.frame = 1;
+                    this.rotation = Math.min(Math.PI / 2, this.rotation + 0.3);
+                }else{
+                    this.rotation = -0.3;
+                }
+                if(currentstate === states.Score){
+
+                }
+            }
+
+
+        },
+
+        render: function (ctx) {
+            ctx.save();
+            ctx.translate(this.x, this.y);
+            ctx.rotate(this.rotation);
+
+            var n = this.animation[this.frame];
+            s_bird[n].draw(ctx, -s_bird[n].width/2, -s_bird[n].height/2);
+
+            if(Boolean(debug)) {
+                ctx.fillStyle = "#f00";
+                ctx.beginPath();
+                ctx.arc(0, 0, this.radius, 0, 2 * Math.PI);
+                ctx.fill();
+            }
+
+            ctx.restore();
+
+        }
+
     },
+
      pipes = {
+
+         _pipes: [],
+
+         reset: function () {
+             this._pipes = [];
+         },
+
+         update: function(){
+            if(frames % 100 === 0) {
+                var _y = height - (s_pipeSouth.height + s_fg.height + 120 + 200 * Math.random());
+                this._pipes.push({
+                    x: 500,
+                    y: _y,
+                    width: s_pipeSouth.width,
+                    height: s_pipeNorth.height
+                });
+            }
+
+            for(var i = 0, len = this._pipes.length; i < len; i++){
+                var p = this._pipes[i];
+
+                // check collision for the first pipe
+                if(i === 0){
+                    var cx = Math.min(Math.max(bird.x, p.x), p.x + p.width);
+                    var cy1 = Math.min(Math.max(bird.y, p.y), p.y + p.height);
+                    var cy2 = Math.min(Math.max(bird.y, p.y + p.height + 80), p.y +  2 * p.height + 80);
+
+                    var dx = bird.x - cx;
+                    var dy1 = bird.y - cy1;
+                    var dy2 = bird.y - cy2;
+
+                    var d1 = dx*dx + dy1*dy1;
+                    var d2 = dx*dx + dy2*dy2;
+
+                    var r = bird.radius * bird.radius;
+
+                    if(r > d1 || r > d2){
+
+                    }
+
+                }
+
+                p.x -= 2;
+                if(p.x < -p.width){
+                    this._pipes.splice(i,1);
+                    i--;
+                    len--;
+                }
+            }
+         },
+
+         render: function(ctx){
+             for(var i = 0, len = this._pipes.length; i < len; i++){
+                 var p = this._pipes[i];
+                 s_pipeSouth.draw(ctx, p.x, p.y);
+                 s_pipeNorth.draw(ctx, p.x, p.y + 80 + p.height);
+             };
+
+             if(Boolean(debug)){
+                 ctx.save();
+                 ctx.fillStyle = "#f00";
+                 ctx.beginPath();
+                 ctx.rect(p.x, p.y, p.width, p.height);
+                 ctx.fill();
+                 ctx.restore();
+             }
+         }
 
      };
 
+function onpress(evt){
+    switch(currentstate){
+        case states.Splash:
+            currentstate = states.Game;
+            bird.jump();
+            break;
+        case states.Game:
+            bird.jump();
+            break;
+        case states.Score:
+            pipes.reset();
+            currentstate = states.Splash;
+            break;
+        default:
+            console.error("onpress.error");
+    }
+}
+
 function main(){
+
     canvas = document.createElement("canvas");
 
     width = window.innerHeight;
     height = window.innerHeight;
 
-    if(width = 500){
+    var evt = "touchstart";
+    if(width >= 500){
         width = 320;
         height = 480;
-        canvas.style.border = "1px solid red":
+        canvas.style.border = "13px dotted white";
+        evt = "mousedown";
     }
+
+    document.addEventListener(evt, onpress);
 
     canvas.width = width;
     canvas.height = height;
+    if (!(!!canvas.getContext && canvas.getContext("2d"))) {
+        alert("Your browser doesn't support HTML5, please update to latest version");
+    }
 
+    ctx = canvas.getContext("2d");
+    currentstate = states.Splash
+    // append canvas to document
     document.body.appendChild(canvas);
+
+    var img = new Image();
+    img.onload = function(){
+        initSprites(this);
+        ctx.fillStyle = s_bg.color;
+        run();
+    }
+
+    img.src = "assets/sheet.png";
 
 }
 
 function run(){
 
+    var loop = function(){
+        update();
+        render();
+        window.requestAnimationFrame(loop, canvas);
+    }
+
+    window.requestAnimationFrame(loop, canvas)
+
 }
 
 function update(){
 
+    frames++;
+
+    if(currentstate === states.Splash){
+        bird.update();
+    }
+    if(currentstate === states.Game){
+        fgpos = (fgpos - 2) % 14;
+        // set best score to maximum score
+        best = Math.max(best, score);
+        localStorage.setItem("best", best);
+        bird.update();
+        pipes.update();
+    }
+    if(currentstate === states.Score){
+
+    }
+
 }
 
 function render(){
+    ctx.fillRect(0,0,width, height);
 
+    // var context = canvas.getContext('2d');
+    // context.clearRect(0,0,canvas.width,canvas.height)
+
+    s_bg.draw(ctx, 0, height - s_bg.height);
+    s_bg.draw(ctx, s_bg.width, height - s_bg.height);
+
+    s_fg.draw(ctx, fgpos, height - s_fg.height);
+    s_fg.draw(ctx, fgpos + s_fg.width, height - s_fg.height);
+
+    var width2 = width / 2;
+
+    if(currentstate === states.Splash){
+        s_splash.draw(ctx, width2 - s_splash.width / 2, height - 300);
+        s_text.GetReady.draw(ctx, width2 - s_text.GetReady.width / 2, height - 380);
+
+        bird.render(ctx);
+    }
+    if(currentstate === states.Game){
+        pipes.render(ctx);
+        bird.render(ctx);
+    }
+    if(currentstate === states.Score){
+        pipes.render(ctx);
+        bird.render(ctx);
+    }
 }
-
-main();
--- js/sprite.js
+++ js/sprite.js
@@ -20,6 +20,7 @@ function Sprite(img, x, y, width, height) {
     this.width = width*2;
     this.height = height*2;
 };
+
 Sprite.prototype.draw = function(ctx, x, y) {
     ctx.drawImage(this.img, this.x, this.y, this.width, this.height,
         x, y, this.width, this.height);
